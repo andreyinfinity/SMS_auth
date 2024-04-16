9f2567bc98b9bd2dcecdbc6ef14f64d87c925464
Andrey Potapov <aapot@ya.ru>
2024-04-16 09:30:00 +0300

    add readme.md

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from users import views
 from users.apps import UsersConfig
 
@@ -11,4 +12,5 @@ urlpatterns = [
     path('login/sms-confirm/', views.SMSConfirmation.as_view(), name='sms-confirm'),
     path('profile/', views.UserProfile.as_view(), name='profile'),
     path('profile/invitation/', views.Invitation.as_view(), name='invitation'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
